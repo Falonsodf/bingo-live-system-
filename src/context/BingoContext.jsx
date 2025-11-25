23f3bd3f58d23b78654ea58c15a2fd771ca94120
Falonsodf <falonso.df@gmail.com>
2025-11-25 13:15:53 -0500

    Update BingoContext.jsx

--- src/context/BingoContext.jsx
+++ src/context/BingoContext.jsx
@@ -1,3 +1,28 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-export const BingoContext = createContext();
+const BingoContext = createContext();
+
+export function BingoProvider({ children }) {
+  const [numbersCalled, setNumbersCalled] = useState([]);
+  const [currentNumber, setCurrentNumber] = useState(null);
+  const [card, setCard] = useState(null);
+
+  const value = {
+    numbersCalled,
+    setNumbersCalled,
+    currentNumber,
+    setCurrentNumber,
+    card,
+    setCard
+  };
+
+  return (
+    <BingoContext.Provider value={value}>
+      {children}
+    </BingoContext.Provider>
+  );
+}
+
+export function useBingo() {
+  return useContext(BingoContext);
+}
